fe7f7bd6bb6d549b6fecadfcfc02dc138784473d
joseph30222 <ochejoseph16@gmail.com>
2025-04-26 14:47:54 +0000

    hardhat config for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    liskSepolia: {
+      url: "https://rpc.sepolia.lisk.com",
+      accounts: [process.env.WALLET_KEY],
+      chainId: 4202,
+    },
+  },
 };
